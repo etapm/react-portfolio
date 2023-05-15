6df647090439e5985fab2ed704bc2666475db8ae
etapm <etap@ucdavis.edu>
2023-05-15 15:36:27 -0700

    Home page edits

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,7 +6,26 @@ function Header() {
   return (
     <header className="header">
       <h1>Edgar Tapia Mendoza</h1>
+      <h3>Software Developer/ Project Manager/ Tech</h3>
       <Navigation />
+      <div className="social-buttons">
+        <a
+          href="https://github.com/etapm"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="social-button"
+        >
+          GitHub
+        </a>
+        <a
+          href="https://linkedin.com/in/etapm"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="social-button"
+        >
+          LinkedIn
+        </a>
+      </div>
     </header>
   );
 }
